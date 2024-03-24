b90e7e8e3dffb37e17904359ae012eb1d8fd255b
ejaltuna <altuna90@gmail.com>
2024-03-24 15:37:58 -0400

    ss

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,13 @@
 import { motion } from "framer-motion";
 
-import { QuoteIcon } from "../assets/icons/QuoteIcon";
 import testimonial1 from "../assets/images/testimonial1.png";
 import testimonial2 from "../assets/images/testimonial2.png";
 import testimonial3 from "../assets/images/testimonial3.png";
-import React from "react";
+
+import { useState } from "react";
 import Carousel from "react-grid-carousel";
+import { InvitationModal } from "./InvitationModal.jsx";
+
 const testimonialsData = [
   {
     customerName: "John Watkins",
@@ -79,70 +81,88 @@ const testimonialsData = [
     image: testimonial3,
     price: "32.00",
   },
-  
 ];
 
-export const Testimonials = () => (
-  <section className="w-full flex justify-center pt-10 mb-16 lg:mb-32 bg-customDarkBg0 relative">
-    <div className="absolute -top-16" id="productos" />
-    <div className="flex flex-col w-full lg:w-[1150px] justify-center">
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.4, delay: 0.3 }}
-      >
-        <div className="custom-block-subtitle text-center mb-6">Catalogo</div>
-        <div className="custom-block-big-title-black text-center mb-16 px-8 sm:px-24 md:px-48">
-          Productos recomendados por <span className="font-bold">HGM</span>
-        </div>
+export const Testimonials = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [producto, setProducto] = useState([]);
+  console.log(producto, "id222");
 
-        <Carousel cols={5} showDots loop>
-          {/* {products.map((val, i) => ( */}
-          {testimonialsData?.map((prod, i) => {
-            return (
-              <Carousel.Item>
-                <div className="bg-white p-4 rounded-lg border">
-                  <div className="mt-1 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-2 lg:grid-cols-1 xl:gap-x-8">
-                    <div className="group relative">
-                      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img
-                          src={prod.url}
-                          alt="Front of men&#039;s Basic Tee in black."
-                          className="h-full w-full object-cover object-center lg:h-full lg:w-full border-b-2"
-                        />
-                      </div>
-                      <div className="mt-4 flex justify-between">
-                        <div>
-                          <h3 className="text-sm text-gray-700">
-                            <a href="#">
-                              <span
-                                aria-hidden="true"
-                                className="absolute inset-0"
-                              ></span>
-                              {prod.customerTitle}
-                            </a>
-                          </h3>
-                          <p className="mt-1 text-sm text-gray-500">
-                            {prod.customerName}
+  return (
+    <section className="w-full flex justify-center pt-10 mb-16 lg:mb-32 bg-customDarkBg0 relative">
+      <div className="absolute -top-16" id="productos" />
+      <div className="flex flex-col w-full lg:w-[1150px] justify-center">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.4, delay: 0.3 }}
+        >
+          <div className="custom-block-subtitle text-center mb-6">Catalogo</div>
+          <div className="custom-block-big-title-black text-center mb-16 px-8 sm:px-24 md:px-48">
+            Productos recomendados por <span className="font-bold">HGM</span>
+          </div>
+
+          <Carousel cols={5} showDots loop>
+            {/* {products.map((val, i) => ( */}
+            {testimonialsData?.map((prod, i) => {
+              return (
+                <Carousel.Item>
+                  <div className="bg-white p-4 rounded-lg border">
+                    <div className="mt-1 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-2 lg:grid-cols-1 xl:gap-x-8">
+                      <div
+                        className="group relative"
+                        onClick={() => [
+                          setIsModalOpen(true),
+                          setProducto(prod),
+                        ]}
+                      >
+                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                          <img
+                            src={prod.url}
+                            alt="Front of men&#039;s Basic Tee in black."
+                            className="h-full w-full object-cover object-center lg:h-full lg:w-full border-b-2"
+                          />
+                        </div>
+                        <div className="mt-4 flex justify-between">
+                          <div>
+                            <h3 className="text-sm text-gray-700">
+                              <a href="#">
+                                <span
+                                  aria-hidden="true"
+                                  className="absolute inset-0"
+                                ></span>
+                                {prod.customerTitle}
+                              </a>
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-500">
+                              {prod.customerName}
+                            </p>
+                          </div>
+                          <p className="text-sm font-medium text-gray-900">
+                            {prod.price}
                           </p>
                         </div>
-                        <p className="text-sm font-medium text-gray-900">
-                          {prod.price}
-                        </p>
                       </div>
+
+                      {/* <!-- More products... --> */}
                     </div>
-
-                    {/* <!-- More products... --> */}
                   </div>
-                </div>
-              </Carousel.Item>
-            );
-          })}
+                </Carousel.Item>
+              );
+            })}
 
-          {/* ))} */}
-        </Carousel>
-      </motion.div>
-    </div>
-  </section>
-);
+            {/* ))} */}
+          </Carousel>
+        </motion.div>
+      </div>
+      {isModalOpen && (
+        <InvitationModal
+          isOpen={isModalOpen}
+          setIsOpen={setIsModalOpen}
+          producto={producto}
+        />
+      )}
+    </section>
+  );
+};
